7ea710b65450854791d34242dad67f66de2ad45b
zahidmiran <zahidmiron599@gmail.com>
2022-05-01 17:55:05 +0600

    delet method add but ..

--- src/components/ManageInventories/AllInventories.js
+++ src/components/ManageInventories/AllInventories.js
@@ -1,32 +1,52 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import useInventories from '../Hooks/useInventories';
 
-const AllInventories = ({ item }) => {
+const AllInventories = (props) => {
+    const{_id, name, price} = props.item;
+    // console.log(_id)
     const [inventoriesItem, setInventoriesItem] = useInventories()
+    
+    // console.log(inventoriesItem._id, 'from all inventoryes')
+    // console.log(item._id, 'from all inventoryes')
 
-    console.log(inventoriesItem._id, 'from all inventoryes')
-    console.log(item._id, 'from all inventoryes')
+    // const deletInventories = id => {
+    //     const proceed = window.confirm('Want to delet Inventory?')
+    //     if (proceed) {
+    //         // const url = `http://localhost:5000//inventory/${id}`
+    //         fetch(`http://localhost:5000/inventory/${id}`, {
+    //             method: 'DELETE',
+    //         })
+    //             .then(res => res.json())
+    //             .then(data => {
+    //                 console.log(data);
+    //                 const remaining = inventoriesItem.filter(item => item._id !== id);
+    //                 setInventoriesItem(remaining);
+    //             })
+    //     }
+    // }
+    const handleDelet = id => {
 
-    const deletInventories = id => {
-        const proceed = window.confirm('Want to delet Inventory?')
+        const proceed = window.confirm('Are you sure??')
         if (proceed) {
-            // const url = `http://localhost:5000//inventory/${id}`
-            fetch(`http://localhost:5000/inventory/${id}`, {
-                method: 'DELETE',
+            const url = `http://localhost:5000/service/${id}`;
+            fetch(url, {
+                method: 'DELETE'
             })
                 .then(res => res.json())
                 .then(data => {
                     console.log(data);
-                    const remaining = inventoriesItem.filter(item => item._id !== id);
-                    setInventoriesItem(remaining);
+                    // const remaining = inventoriesItem.filter(service => service._id !== id);
+                    // setInventoriesItem(remaining);
                 })
         }
     }
 
     return (
         <div>
-            <Card style={{ width: '18rem' }}>
+            {/* <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={item.imageURL} />
                 <Card.Body>
                     <Card.Title>Card Title</Card.Title>
@@ -39,7 +59,16 @@ const AllInventories = ({ item }) => {
                         
                     }
                 </Card.Body>
-            </Card>
+            </Card> */}
+             <div className="m-1 mb-4 col-md-3">
+            <ul className="list-group">
+                <li className="list-group-item list-group-item-dark"><span className="fw-bolder text-dark"> Name : {name} </span></li>
+                <li className="list-group-item "><span className="fw-bolder text-dark">Price : $-{price} </span> </li>
+                {/* <li className="list-group-item "><span className="fw-bolder text-dark">Food Weight : {weight} Kg</span> </li> */}
+                <li onClick={() => handleDelet(_id)} className="list-group-item "><span className="btn btn-outline-danger fw-bolder text-dark"><FontAwesomeIcon icon={faTrashAlt} />  Delete Food</span></li>
+
+            </ul>
+        </div>
         </div>
     );
 };
--- src/components/ManageInventories/ManageInventories.js
+++ src/components/ManageInventories/ManageInventories.js
@@ -1,48 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import useInventories from '../Hooks/useInventories';
 import AllInventories from './AllInventories';
 
 const ManageInventories = () => {
-    const [inventoriesItem, setInventoriesItem] = useInventories();
-    const navigate = useNavigate();  
 
-     const handleDelet = id =>{
+    const [inventoriesItem, setInventoriesItem] = useInventories();
+    const navigate = useNavigate();
+
+    const handleDelet = id => {
         const proceed = window.confirm('Are you sure??')
-        if(proceed){
+        if (proceed) {
             const url = `http://localhost:5000/service/${id}`;
             fetch(url, {
                 method: 'DELETE'
             })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                const remaining = inventoriesItem.filter(service => service._id !== id);
-                setInventoriesItem(remaining);
-            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = inventoriesItem.filter(service => service._id !== id);
+                    setInventoriesItem(remaining);
+                })
         }
     }
+    const [inventorys, setInventorys] = useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/inventories')
+        .then(res=>res.json())
+        .then(data => setInventorys(data));
+    },[inventorys])
     return (
-        <div>
+        <div style={{ backgroundColor: "#12161f", color: "white" }}>
             <p>  Inventory Items:{inventoriesItem.length}</p>
             <h1>All Inventories</h1>
 
-            <div className='container'>
-                <div className='row'>
-                    <div className='inventory-cards'>
-                        {/* {
-                        inventoriesItem?.map(item => <AllInventories key={inventoriesItem._id} item={item}></AllInventories>)
-                        } */}
-                        {
-                inventoriesItem.map(service => <div key={service._id}>
-                    <h6>{service.name} <button onClick={() => handleDelet(service._id)}>Delet</button> </h6>
-                </div> )
-            }
-                    </div>
 
+            <div className="text-center mt-5 pt-5">
+                <h1 className="text-center mt-5 pt-5">Total  {inventoriesItem.length} Inventories!!!!!</h1>
+                <div className="row d-flex justify-content-center">
+                    {
+                        inventorys.map(item => <AllInventories key={item._id} id={item._id} item={item}></AllInventories>)
+                    }
                 </div>
             </div>
+
             <Button onClick={() => navigate('/addinventories')} className=''>add new Inventories</Button>
 
         </div>
